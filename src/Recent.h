a8910369788b3f3fd96a659e14d36f85ab3bd633
Toni Gundogdu <legatvs@gmail.com>
2011-10-18 18:25:58 +0300

    Add email

--- src/Recent.h
+++ src/Recent.h
@@ -1,6 +1,6 @@
 /*
  * NomNom
- * Copyright (C) 2010 Toni Gundogdu.
+ * Copyright (C) 2010  Toni Gundogdu <legatvs@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
